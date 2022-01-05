33e80c88f63e3015440a7d56ca17b946e9564b34
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-01-05 14:34:40 +0100

    Adding wrapper.h wrapper.cpp to simplify network connection

---
+++ include/echoserver.h
@@ -0,0 +1,10 @@
+#ifndef ASIO_NETWORK_PROGRAMMING_ECHOSERVER_H
+#define ASIO_NETWORK_PROGRAMMING_ECHOSERVER_H
+
+#include "wrapper.h"
+#include <conio.h>
+#include <boost/thread/mutex.hpp>
+
+
+
+#endif //ASIO_NETWORK_PROGRAMMING_ECHOSERVER_H
